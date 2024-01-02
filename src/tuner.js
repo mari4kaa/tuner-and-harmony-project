c24634cbef14511ccd8aa13d955d1b4adef39fff
Mariia Kryvokhata <m.krivokhata@gmail.com>
2024-01-02 20:24:03 +0200

    Remove prototypes

--- src/tuner.js
+++ src/tuner.js
@@ -1,5 +1,12 @@
 'use strict';
 
+const findNeighbour = function(frequency, prev, next) {
+  const deltaPrev = frequency - prev;
+  const deltaNext = next - frequency;
+  const neighbour = (deltaPrev > deltaNext) ? next : prev;
+  return neighbour;
+};
+
 class Tuner {
   constructor(defaultConfig) {
     this.config = defaultConfig;
@@ -7,20 +14,7 @@ class Tuner {
     this.OCTAVES = 4;
     this.MIN_DECIBELS = -50;
     this.TOTAL_NOTES_COUNT = 12;
-    this.AllNotes = [
-      'C',
-      'C#',
-      'D',
-      'D#',
-      'E',
-      'F',
-      'F#',
-      'G',
-      'G#',
-      'A',
-      'A#',
-      'B',
-    ];
+    this.AllNotes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
     this.StandardTune = [
       ['E', 2],
       ['A', 2],
@@ -30,172 +24,166 @@ class Tuner {
       ['E', 4],
     ];
   }
-}
 
-Tuner.prototype.init = async function() {
-  window.AudioContext = window.AudioContext || window.webkitAudioContext;
-  this.audioCtx = new window.AudioContext();
-  this.analyser = new AnalyserNode(this.audioCtx);
-  this.scriptProcessor = this.audioCtx.createScriptProcessor(
-    4096,
-    1,
-    1,
-  );
-  this.analyser.connect(this.scriptProcessor);
-  this.scriptProcessor.connect(this.audioCtx.destination);
-  try {
-    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-    window.localStream = stream;
-    this.audioCtx.createMediaStreamSource(stream).connect(this.analyser);
-  } catch (err) {
-    console.error(`Error: ${err}`);
+  async init() {
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    this.audioCtx = new window.AudioContext();
+    this.analyser = new AnalyserNode(this.audioCtx);
+    this.scriptProcessor = this.audioCtx.createScriptProcessor(
+      4096,
+      1,
+      1,
+    );
+    this.analyser.connect(this.scriptProcessor);
+    this.scriptProcessor.connect(this.audioCtx.destination);
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+      window.localStream = stream;
+      this.audioCtx.createMediaStreamSource(stream).connect(this.analyser);
+    } catch (err) {
+      console.error(`Error: ${err}`);
+    }
   }
-};
 
-Tuner.prototype.tune = function(onCapturedCb) {
-  this.scriptProcessor.addEventListener('audioprocess', () => {
-    const userFrequency = this.getUserFrequency();
-    if (userFrequency) {
-      let noteData;
-      if (this.config.mode !== 'standardStrict') {
-        noteData = this.autoModes(userFrequency, this.config.mode);
-      } else {
-        const stringIdx = this.StandardTune.length - this.config.selectedString;
-        noteData = this.modeStandardStrict(userFrequency, stringIdx);
+  tune(onCapturedCb) {
+    this.scriptProcessor.addEventListener('audioprocess', () => {
+      const userFrequency = this.getUserFrequency();
+      if (userFrequency) {
+        let noteData;
+        if (this.config.mode !== 'standardStrict') {
+          noteData = this.autoModes(userFrequency, this.config.mode);
+        } else {
+          const stringIdx = this.StandardTune.length - this.config.selectedString;
+          noteData = this.modeStandardStrict(userFrequency, stringIdx);
+        }
+        onCapturedCb(noteData);
       }
-      onCapturedCb(noteData);
-    }
-  });
-};
+    });
+  }
 
-Tuner.prototype.getUserFrequency = function() {
-  const bufferLength = this.analyser.frequencyBinCount;
-  const dataArray = new Float32Array(bufferLength);
-  this.analyser.getFloatFrequencyData(dataArray);
+  getUserFrequency() {
+    const bufferLength = this.analyser.frequencyBinCount;
+    const dataArray = new Float32Array(bufferLength);
+    this.analyser.getFloatFrequencyData(dataArray);
 
-  const maxVolumeIndex = dataArray.indexOf(Math.max(...dataArray));
-  const binWidth = this.audioCtx.sampleRate / this.analyser.fftSize;
-  const userFrequency = maxVolumeIndex * binWidth;
-  if (dataArray[maxVolumeIndex] > this.MIN_DECIBELS) return userFrequency;
-  return undefined;
-};
+    const maxVolumeIndex = dataArray.indexOf(Math.max(...dataArray));
+    const binWidth = this.audioCtx.sampleRate / this.analyser.fftSize;
+    const userFrequency = maxVolumeIndex * binWidth;
+    if (dataArray[maxVolumeIndex] > this.MIN_DECIBELS) return userFrequency;
+    return undefined;
+  }
 
-Tuner.prototype.noteNum = function(frequency) {
-  const octaveCoef = Math.log(frequency / this.START_FREQUENCY) / Math.log(2);
-  const noteNum = this.TOTAL_NOTES_COUNT * octaveCoef;
-  return Math.round(noteNum);
-};
+  noteNum(frequency, startFrequency, notesCount) {
+    const octaveCoef = Math.log(frequency / startFrequency) / Math.log(2);
+    const noteNum = notesCount * octaveCoef;
+    return Math.round(noteNum);
+  }
 
-Tuner.prototype.standardNoteNum = function(noteName, noteOctave) {
-  const noteIdx = this.AllNotes.indexOf(noteName);
-  const relativeOctave = noteOctave - 1;
-  const noteNum = noteIdx + this.TOTAL_NOTES_COUNT * (relativeOctave - 1);
-  return noteNum;
-};
+  standardNoteNum(noteName, noteOctave) {
+    const noteIdx = this.AllNotes.indexOf(noteName);
+    const relativeOctave = noteOctave - 1;
+    const noteNum = noteIdx + this.TOTAL_NOTES_COUNT * (relativeOctave - 1);
+    return noteNum;
+  }
 
-Tuner.prototype.getOctave = function(noteNum) {
-  const octave = Math.ceil(noteNum / this.TOTAL_NOTES_COUNT) + 1;
-  return octave;
-};
+  getOctave(noteNum) {
+    const octave = Math.ceil(noteNum / this.TOTAL_NOTES_COUNT) + 1;
+    return octave;
+  }
 
-Tuner.prototype.getNoteFrequency = function(noteNum) {
-  const frequencyCoef = Math.pow(2, noteNum / this.TOTAL_NOTES_COUNT);
-  const frequency = this.START_FREQUENCY * frequencyCoef;
-  return frequency;
-};
+  getNoteFrequency(noteNum) {
+    const frequencyCoef = Math.pow(2, noteNum / this.TOTAL_NOTES_COUNT);
+    const frequency = this.START_FREQUENCY * frequencyCoef;
+    return frequency;
+  }
 
-Tuner.prototype.targetFrequencyAllNotes = function(frequency) {
-  const minFreq = this.getNoteFrequency(0);
-  const maxFreq = this.getNoteFrequency((this.AllNotes.length * this.OCTAVES) - 1);
+  targetFrequencyAllNotes(frequency) {
+    const minFreq = this.getNoteFrequency(0);
+    const maxFreq = this.getNoteFrequency((this.AllNotes.length * this.OCTAVES) - 1);
 
-  if (frequency <= minFreq) return minFreq;
-  if (frequency >= maxFreq) return maxFreq;
+    if (frequency <= minFreq) return minFreq;
+    if (frequency >= maxFreq) return maxFreq;
 
-  const allNotes = this.AllNotes.length * this.OCTAVES;
-  for (let i = 0; i < allNotes - 1; i++) {
-    const prev = this.getNoteFrequency(i);
-    const next = this.getNoteFrequency(i + 1);
-    if (frequency >= prev && frequency <= next) {
-      const targetFrequency = this.findNeighbour(frequency, prev, next);
-      return targetFrequency;
+    const allNotes = this.AllNotes.length * this.OCTAVES;
+    for (let i = 0; i < allNotes - 1; i++) {
+      const prev = this.getNoteFrequency(i);
+      const next = this.getNoteFrequency(i + 1);
+      if (frequency >= prev && frequency <= next) {
+        const targetFrequency = findNeighbour(frequency, prev, next);
+        return targetFrequency;
+      }
     }
   }
-};
 
-Tuner.prototype.targetFrequencyStandard = function(frequency) {
-  const length = this.StandardTune.length;
-  const numOfMin = this.standardNoteNum(...this.StandardTune[0]);
-  const numOfMax = this.standardNoteNum(...this.StandardTune[length - 1]);
+  targetFrequencyStandard(frequency) {
+    const length = this.StandardTune.length;
+    const numOfMin = this.standardNoteNum(...this.StandardTune[0]);
+    const numOfMax = this.standardNoteNum(...this.StandardTune[length - 1]);
 
-  const minFreq = this.getNoteFrequency(numOfMin);
-  const maxFreq = this.getNoteFrequency(numOfMax);
+    const minFreq = this.getNoteFrequency(numOfMin);
+    const maxFreq = this.getNoteFrequency(numOfMax);
 
-  if (frequency <= minFreq) return minFreq;
-  if (frequency >= maxFreq) return maxFreq;
+    if (frequency <= minFreq) return minFreq;
+    if (frequency >= maxFreq) return maxFreq;
 
-  for (let i = 0; i < length - 1; i++) {
-    const standardNotePrev = this.StandardTune[i];
-    const standardNoteNext = this.StandardTune[i + 1];
-    const [prev, next] = this.standardFreqInterval(standardNotePrev, standardNoteNext);
-    if (frequency >= prev && frequency <= next) {
-      const targetFrequency = this.findNeighbour(frequency, prev, next);
-      return targetFrequency;
+    for (let i = 0; i < length - 1; i++) {
+      const standardNotePrev = this.StandardTune[i];
+      const standardNoteNext = this.StandardTune[i + 1];
+      const [prev, next] = this.standardFreqInterval(standardNotePrev, standardNoteNext);
+      if (frequency >= prev && frequency <= next) {
+        const targetFrequency = findNeighbour(frequency, prev, next);
+        return targetFrequency;
+      }
     }
   }
-};
 
-Tuner.prototype.findNeighbour = function(frequency, prev, next) {
-  const deltaPrev = frequency - prev;
-  const deltaNext = next - frequency;
-  const neighbour = (deltaPrev > deltaNext) ? next : prev;
-  return neighbour;
-};
+  standardFreqInterval(standardNotePrev, standardNoteNext) {
+    const standardNumPrev = this.standardNoteNum(...standardNotePrev);
+    const standardNumNext = this.standardNoteNum(...standardNoteNext);
+    const prev = this.getNoteFrequency(standardNumPrev);
+    const next = this.getNoteFrequency(standardNumNext);
 
-Tuner.prototype.standardFreqInterval = function(standardNotePrev, standardNoteNext) {
-  const standardNumPrev = this.standardNoteNum(...standardNotePrev);
-  const standardNumNext = this.standardNoteNum(...standardNoteNext);
-  const prev = this.getNoteFrequency(standardNumPrev);
-  const next = this.getNoteFrequency(standardNumNext);
+    return [prev, next];
+  }
 
-  return [prev, next];
-};
+  autoModes(userFrequency, mode) {
+    const targetFrequency = (mode === 'allNotes') ?
+      this.targetFrequencyAllNotes(userFrequency) :
+      this.targetFrequencyStandard(userFrequency);
 
-Tuner.prototype.autoModes = function(userFrequency, mode) {
-  const targetFrequency = (mode === 'allNotes') ?
-    this.targetFrequencyAllNotes(userFrequency) :
-    this.targetFrequencyStandard(userFrequency);
+    const deltaFreq = userFrequency - targetFrequency;
+    const noteNum = this.noteNum(targetFrequency);
+    const noteName = this.AllNotes[noteNum % this.TOTAL_NOTES_COUNT];
 
-  const deltaFreq = userFrequency - targetFrequency;
-  const noteNum = this.noteNum(targetFrequency);
-  const noteName = this.AllNotes[noteNum % this.TOTAL_NOTES_COUNT];
+    const noteData = {
+      delta: deltaFreq,
+      name: noteName,
+      octave: this.getOctave(noteNum),
+    };
 
-  const noteData = {
-    delta: deltaFreq,
-    name: noteName,
-    octave: this.getOctave(noteNum),
-  };
+    console.log(noteData);
 
-  console.log(noteData);
+    return noteData;
+  }
 
-  return noteData;
-};
+  modeStandardStrict(userFrequency, stringIdx) {
+    const standardNote = this.StandardTune[stringIdx];
+    const [noteName, noteOctave] = standardNote;
+    const noteNum = this.standardNoteNum(noteName, noteOctave);
+    const targetFrequency = this.getNoteFrequency(noteNum);
+    const deltaFreq = userFrequency - targetFrequency;
 
-Tuner.prototype.modeStandardStrict = function(userFrequency, stringIdx) {
-  const standardNote = this.StandardTune[stringIdx];
-  const [noteName, noteOctave] = standardNote;
-  const noteNum = this.standardNoteNum(noteName, noteOctave);
-  const targetFrequency = this.getNoteFrequency(noteNum);
-  const deltaFreq = userFrequency - targetFrequency;
+    const noteData = {
+      delta: deltaFreq,
+      name: noteName,
+      octave: noteOctave,
+    };
 
-  const noteData = {
-    delta: deltaFreq,
-    name: noteName,
-    octave: noteOctave,
-  };
+    console.log(noteData);
 
-  console.log(noteData);
+    return noteData;
+  }
 
-  return noteData;
-};
+}
 
 module.exports = Tuner;
--- src/user-interface/UI-tuner/meter.js
+++ src/user-interface/UI-tuner/meter.js
@@ -8,38 +8,38 @@ class Meter {
     this.pointer = undefined;
     this.createScale();
   }
-}
 
-Meter.prototype.createScale = function() {
-  const containerWidth = this.container.offsetWidth;
-  const lineGap = containerWidth / this.NUM_OF_LINES;
+  createScale() {
+    const containerWidth = this.container.offsetWidth;
+    const lineGap = containerWidth / this.NUM_OF_LINES;
 
-  for (let i = 0; i < this.NUM_OF_LINES; i++) {
-    const line = document.createElement('div');
-    line.classList.add('scale-line');
-    line.style.left = `${lineGap * i}px`;
+    for (let i = 0; i < this.NUM_OF_LINES; i++) {
+      const line = document.createElement('div');
+      line.classList.add('scale-line');
+      line.style.left = `${lineGap * i}px`;
 
-    this.container.appendChild(line);
+      this.container.appendChild(line);
+    }
+
+    const centralLine = document.createElement('div');
+    centralLine.classList.add('central-line');
+    centralLine.style.left = `${containerWidth / 2}px`;
+    this.container.appendChild(centralLine);
+
+    this.pointer = document.createElement('div');
+    this.pointer.classList.add('pointer');
+    this.container.appendChild(this.pointer);
+
+    const metricElement = document.getElementById('metric');
+    const metric = Math.round(this.container.offsetWidth / this.NUM_OF_LINES);
+    metricElement.textContent = `1 line ~ ${metric} Hz`;
   }
 
-  const centralLine = document.createElement('div');
-  centralLine.classList.add('central-line');
-  centralLine.style.left = `${containerWidth / 2}px`;
-  this.container.appendChild(centralLine);
-
-  this.pointer = document.createElement('div');
-  this.pointer.classList.add('pointer');
-  this.container.appendChild(this.pointer);
-
-  const metricElement = document.getElementById('metric');
-  const metric = Math.round(this.container.offsetWidth / this.NUM_OF_LINES);
-  metricElement.textContent = `1 line ~ ${metric} Hz`;
-};
-
-Meter.prototype.update = function(delta) {
-  const maxDelta = this.container.offsetWidth / 2 - this.BORDER;
-  const pointerPosition = delta >= maxDelta ? maxDelta : delta;
-  this.pointer.style.transform = `translateX(${pointerPosition}px)`;
-};
+  update(delta) {
+    const maxDelta = this.container.offsetWidth / 2 - this.BORDER;
+    const pointerPosition = delta >= maxDelta ? maxDelta : delta;
+    this.pointer.style.transform = `translateX(${pointerPosition}px)`;
+  }
+}
 
 module.exports = Meter;
